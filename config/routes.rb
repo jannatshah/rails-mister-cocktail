6be5bdd6e8bc78816a4832bf569740de2a38ea1a
Jannat Shah <jannat.shah@gmail.com>
2019-11-14 16:57:29 +0000

    Complete backend with Routing, Controller, Views for Doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'cocktails', to: 'cocktails#index'
-  get 'cocktails/new', to: 'cocktails#new'
+  # get 'cocktails', to: 'cocktails#index'
+  # get 'cocktails/new', to: 'cocktails#new'
 
-  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
-  post 'cocktails', to: 'cocktails#create'
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  # post 'cocktails', to: 'cocktails#create'
+
+  resources :cocktails, only: [:index, :new, :show, :create] do
+    resources :doses, only: [:index, :new, :create]
+  end
+
+  resources :doses, only: :destroy
 end
